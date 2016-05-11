9651de0040b045794d4b308ccdca49563d5b9717
Ponkin <alexey.ponkin@yandex.ru>
2016-05-12 00:00:11 +0300

    added tcp envelope(full with crc32)

--- cli.hs
+++ cli.hs
@@ -7,11 +7,12 @@ import Data.Time.Clock.POSIX
 import System.Locale
 import Data.Ratio
 import System.Entropy
+import Data.Digest.CRC32
 
 req_pq :: B.ByteString -> Put
 req_pq nonce = do
-              putInt32le 1615239032
-              putByteString $ nonce
+        putInt32le 1615239032
+        putByteString $ nonce
 
 wrap :: Integer -> B.ByteString -> Put
 wrap exactUnixTime bytes = do
@@ -20,8 +21,19 @@ wrap exactUnixTime bytes = do
       putInt32le $ fromIntegral $ B.length bytes
       putByteString $ bytes
 
+calcCrc32 :: B.ByteString -> Put
+calcCrc32 bytes = do
+           putByteString $ bytes
+           putWord32le $ crc32 bytes
+
+tcp_pack_full :: Integer -> B.ByteString -> Put
+tcp_pack_full seqNum bytes = calcCrc32 $ BL.toStrict . runPut $ do
+      putInt32le $ fromIntegral $ 12 + B.length bytes
+      putInt32le $ fromIntegral $ seqNum
+      putByteString $ bytes
+
 main :: IO ()
 main = do
      time <- getPOSIXTime
      key <- getEntropy 16
-     BL.putStr $ runPut $ wrap (numerator . toRational . (* 4096000000) $ time) (BL.toStrict . runPut $ req_pq key)
+     BL.putStr $ runPut $ tcp_pack_full 0 $ (BL.toStrict . runPut $ wrap (numerator . toRational . (* 4096000000) $ time) (BL.toStrict . runPut $ req_pq key))
