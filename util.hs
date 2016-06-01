bee347b2bade2213a1c2bd67f4baf09326de26f6
Ponkin <alexey.ponkin@yandex.ru>
2016-06-01 12:38:10 +0300

    introduce Util module

--- util.hs
+++ util.hs
@@ -3,12 +3,12 @@ module Util
 , pollard_rho
 ) where 
 	
-pollardStep :: (Integer -> Integer) -> Integer -> Integer -> Integer -> Integer
-pollardStep f n x y
+pollard_rho_step :: (Integer -> Integer) -> Integer -> Integer -> Integer -> Integer
+pollard_rho_step f n x y
       | d > 1 = d
       | otherwise = let x1 = f x
                         y1 = f . f $ y
-                    in pollardStep f n x1 y1
+                    in pollard_rho_step f n x1 y1
       where d = gcd n $ abs $ x - y
 
 pollard_rho :: Integer -> Integer
@@ -17,17 +17,14 @@ pollard_rho n =
         x0 = f 2
         y0 = f . f $ 2
     in pollardStep f n x0 y0
-	
-func :: Integer -> Integer -> Integer
-func x n = mod ( x * x - 1) n
 
-pollardStep1 :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
-pollardStep1 i k n x y
+pollard_rho_brent_step :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+pollard_rho_brent_step i k n x y
       | d /= 1 && d /= n = d
-      | i == k = pollardStep1 (i+1) (2*k) n x1 x1
-      | otherwise = pollardStep1 (i+1) k n x1 y
+      | i == k = pollard_rho_brent_step (i+1) (2*k) n x1 x1
+      | otherwise = pollard_rho_brent_step (i+1) k n x1 y
       where d = gcd n $ abs $ y - x
-            x1 = func x n
+            x1 = mod ( x * x - 1) n
 
 pollard_rho_brent :: Integer -> Integer
-pollard_rho_brent n = pollardStep1 1 2 n 2 2
+pollard_rho_brent n = pollard_rho_brent_step 1 2 n 2 2
